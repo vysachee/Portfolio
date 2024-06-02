2604ac82933bdf342be694b91ad3f8271de509df
vysachee <toms.d27@gmail.com>
2024-06-02 22:53:33 +0200

    Update Menu.tsx

--- website/components/menu/Menu.tsx
+++ website/components/menu/Menu.tsx
@@ -12,35 +12,9 @@ const menuLinks = [
   { path: "/contact", label: "Contacts." },
 ];
 
-function animateLinkHover2(selector: string, width: string) {
-      const socialLink = document.querySelector(selector);
-
-      if (socialLink) {
-          const line = socialLink.querySelector(".underline2");
-
-          const handleMouseEnter = () => {
-              gsap.to(line, { width: width, duration: 0.5, ease: "power2.out" });
-          };
-
-          const handleMouseLeave = () => {
-              gsap.to(line, { width: "0%", duration: 0.5, ease: "power2.out" });
-          };
-
-          socialLink.addEventListener("mouseenter", handleMouseEnter);
-          socialLink.addEventListener("mouseleave", handleMouseLeave);
-
-          return () => {
-              socialLink.removeEventListener("mouseenter", handleMouseEnter);
-              socialLink.removeEventListener("mouseleave", handleMouseLeave);
-          };
-      }
-}
 
 const Menu = () => {
-  
-  animateLinkHover2(".twitter", "35%");
-  animateLinkHover2(".instagram", "35%");
-  animateLinkHover2(".linkedin", "35%");
+
 
   const container = useRef<HTMLDivElement>(null);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
